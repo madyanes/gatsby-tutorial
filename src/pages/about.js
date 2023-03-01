8c95b451ca95c22f9449b56f021819b7400b8022
madyanes <madyanes.1995@gmail.com>
2023-03-02 06:34:33 +0700

    create new react components

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Ian is a self-taught website developer from Indonesia.</p>
+    </div>
+  )
+}
+
+export default About
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <p>You can find me on Twitter via @ingatdanwaspada.</p>
+    </div>
+  )
+}
+
+export default Contact
